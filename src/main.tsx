ef0da1063bf114c7808b9b87420f70be3ad5ff52
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-06 23:00:14 +0900

    CHORE: main.tsx에 css 연동 및 index.html css 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import "./css/style.css";
+import "./css/components.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/eslint-kr/">
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
