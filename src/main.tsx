6469bb278f7138227ecac33d13466f540411e3c8
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-04-06 22:56:18 +0900

    CHORE: 배포 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import './css/components.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/eslint-kr/">
       <App />
     </BrowserRouter>
   </StrictMode>,
